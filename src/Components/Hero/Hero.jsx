cb4b4091327685da276d7ec0baf1493ed695efc9
shaz.ahmed <steve.josh@kanrad.com>
2024-09-24 12:49:00 +0530

    enable clicking in hero section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ import profile_img from '../../assets/profile_img.svg'
 
 const Hero = () => {
     return (
-        <div className='hero'>
+        <div id='home' className='hero'>
             <img src={profile_img} alt="" />
             <h1><span> I'm Shaz Ahmed,</span> a full-stack developer based in India</h1>
             <p>I specialize in building dynamic and scalable web applications, focusing on both backend and frontend development. My passion lies in crafting clean, efficient code and delivering seamless user experiences.</p>
