9665d4ef4b29a734691b16e5abb1f12ff847d341
shaz.ahmed <steve.josh@kanrad.com>
2024-09-24 18:50:05 +0530

    coorected my service order

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -1,23 +1,23 @@
 const Services_Data = [
     {
         s_no: "01",
-        s_name: "React Project",
-        s_desc: "Building dynamic and responsive web applications using React.js."
-    },
-    {
-        s_no: "02",
-        s_name: "Next.js Project",
-        s_desc: "Developing scalable and optimized server-side rendered applications with Next.js."
-    },
-    {
-        s_no: "03",
         s_name: "API using .NET",
         s_desc: "Creating robust and secure REST APIs using .NET technology."
     },
     {
-        s_no: "04",
+        s_no: "02",
         s_name: "Database Design",
         s_desc: "Designing and optimizing relational databases for scalable applications."
+    },
+    {
+        s_no: "03",
+        s_name: "React Project",
+        s_desc: "Building dynamic and responsive web applications using React.js."
+    },
+    {
+        s_no: "04",
+        s_name: "Next.js Project",
+        s_desc: "Developing scalable and optimized server-side rendered applications with Next.js."
     }
 ];
 
